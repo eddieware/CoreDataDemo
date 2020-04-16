f36f4fc55964127bff195439411ed9c0e58256e2
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2020-04-16 15:39:31 -0500

    update

--- CoreDataDemo/ContentView.swift
+++ CoreDataDemo/ContentView.swift
@@ -9,8 +9,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    //recuperar los entities de Student a trave de property wraoer, sort descriptor =  WHERE //regresa FETCHES Result
+    //@EVERYTHING Variables globales dela aplicacion
+    @Environment(\.managedObjectContext) var moc //para el objeto hacerlo contexto
+    @FetchRequest(entity: Student.entity(), sortDescriptors:[])  var students: FetchedResults<Student> //el moc es para manipular el MOC
+    // property wraper = Fetxh Request
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            List{
+                ForEach(students, id:\.id){student in
+                    Text(student.name ?? "Unknown")
+                }
+                
+            }
+            Button("Add"){
+                let firstName = ["Alonso","Adriana","Luna", "Jose"]
+                let lastName = ["Gonzalez","Perez","Garcia","Lopez"]
+                let chosenFirstName = firstName.randomElement()!//forzar a regresar datos
+                let chosenLastName = lastName.randomElement()!
+                
+                let student = Student(context: self.moc) //contexto para refrescar cambios en entidades
+                student.id = UUID()
+                student.name = "\(chosenFirstName) \(chosenLastName)" //concatenar valores e insertar espacio en blanco
+                try? self.moc.save()// guardar cambios efectuados antes de salir :)
+                
+            }
+        }
     }
 }
 
